0c262465ebc6b6d6ab58df01559683f453686d2d
Jhoan Eduardo Saldarriaga Serna <jhoan.saldarriaga@correounivalle.edu.co>
2025-10-24 17:52:37 -0500

    Corregir nombre de paquete y configurar despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ¡IMPORTANTE! Cambia '<TU_REPOSITORIO>' por el nombre de tu repo.
-  // Por ejemplo, si tu repo es 'cotizador-offset', la línea sería: base: '/cotizador-offset/',
-  base: '/<OFFSET_CALCULATOR>/', 
+  base: '/OFFSET_CALCULATOR/', 
 });
