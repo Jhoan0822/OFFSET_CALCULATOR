c3609e6c7ea394c67daf2e545486023297a411a5
Jhoan Saldarriaga <57457751+Jhoan0822@users.noreply.github.com>
2025-10-24 17:32:35 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+// vite.config.ts
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // Esto cargará las variables de .env, .env.local, y para 'production', también .env.production
+    const env = loadEnv(mode, process.cwd(), '');
+
     return {
+      // ---> AÑADE ESTA LÍNEA AQUÍ <---
+      // Usa la variable de entorno para la ruta base, o '/' como valor por defecto para desarrollo.
+      base: env.VITE_BASE_URL || '/',
+
       server: {
         port: 3000,
         host: '0.0.0.0',
